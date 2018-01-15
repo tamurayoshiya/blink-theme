c7214c8b8291e8e3feb4d5d9f88c8e45d74d6112
TAMURA Yoshiya <a@qmu.jp>
2018-01-15 20:35:17 +0900

    Add mono.js

---
+++ mono.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#ffffff"]);
+t.prefs_.set('foreground-color', "#000000");
+t.prefs_.set('background-color', "#ffffff");
+t.prefs_.set('cursor-color', 'rgba(187,147,187,0.5)');
--- plain.js
+++
@@ -1,3 +0,0 @@
-t.prefs_.set('foreground-color', "#000000");
-t.prefs_.set('background-color', "#ffffff");
-t.prefs_.set('cursor-color', 'rgba(127,187,187,0.5)');
